99355ac7f6fb1b80da99918cbf3614e437c275fc
bhagya-patel <imbhagya2005@gmail.com>
2025-12-25 22:52:54 +0530

    Define input variables for AWS infrastructure

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ec2_instance_type" {
+    type        = string
+    default     = "t3.micro"
+}
+
+variable "ec2_ami_id" {
+    type        = string
+    default     = "ami-0ecb62995f68bb549"  # Ubuntu AMI
+}
+
+variable "ec2_root_volume_size" {
+    type        = number
+    default     = 15
+}
+
+
