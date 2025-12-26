554d52dfa2ce550e091447fce76df30311869b78
bhagya-patel <imbhagya2005@gmail.com>
2025-12-26 23:11:55 +0530

    Add outputs for EC2 instance details

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,28 @@
+# Outputs for count
+
+# output ec2_public_ip {
+#     value = aws_instance.my_instance[*].public_ip
+# }
+
+# output ec2_public_dns {
+#     value = aws_instance.my_instance[*].public_dns
+# }
+
+#___________________________________________________________________
+
+# Outputs for for_each
+
 output ec2_public_ip {
-    value = aws_instance.my_instance.public_ip
+    value = [
+        for key in aws_instance.my_instance : key.public_ip
+    ]
 }
 
 output ec2_public_dns {
-    value = aws_instance.my_instance.public_dns
+    value = [
+        for key in aws_instance.my_instance : key.public_dns
+    ]
 }
+
+#___________________________________________________________________
+
