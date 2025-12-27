f25cae34c84bec5d3e4eec07b7ada02790f7a442
bhagya-patel <imbhagya2005@gmail.com>
2025-12-27 21:07:05 +0530

    Add Terraform configuration to provision EC2 instance

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,15 @@ terraform {
       version = "6.27.0"
     }
   }
+
+  #for a remote infra
+
+backend "s3" {
+  bucket         = "bhagya1-bucket"
+  key            = "terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "bhagya1-table"
+  
+}
+
 }
